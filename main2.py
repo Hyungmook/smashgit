c1b423812c707c2071ba700d485d4aaf45a7b75f
Your Name <hyusteoh@umich.edu>
2016-12-01 14:06:19 -0600

    More changes
    Developing another code so that I could now have a base. This should be
    the best reference, I moved around way too much

--- main2.py
+++ main2.py
@@ -21,9 +21,6 @@ everything = pg.sprite.Group()
 FPS = 60
 fpsClock = pg.time.Clock()
 
-#Making the tab
-DISPLAYSURF = pg.display.set_mode((1000, 700))
-
 
 #***TEST***This is to draw a black line from x coordinate 200-800 and y coordinate 400
 #pg.draw.line(DISPLAYSURF, black, (800, 400), (200, 400))
@@ -38,38 +35,84 @@ BALL_Y = 200
 
 #Creating a hand that DEFLECTS NOW MOTHER FUQER
 class MasterHand(pg.sprite.Sprite):
-	def __init__(self, hand):
-		super(MasterHand, self).__init__()
-		self.image = pg.image.load('hohversion/handopen.png')
-		self.rect = self.image.get_rect()
-		self.rect.center = (HAND_X, HAND_Y)
-		self.x = 0
-		self.y = 0
-		self.handx = 15
-		self.handy = 15
+    def __init__(self, x, y, width, height):
+        super(MasterHand, self).__init__()
+        self.image = pg.image.load('hohversion/handopen.png')
+        self.rect = self.image.get_rect()
+        self.rect.center = (HAND_X, HAND_Y)
+        self.x = 0
+        self.y = 0
+        self.handx = 15
+        self.handy = 15
 
-	def hit_position(self, ball):
-		virtual_height = self.height + ball.height
-		y_dist = ball.y + ball.height - self.y
-		pct = y_dist / float(virtual_height)
-		return pct
+        self.width = 10
+        self.height = 75
+
+	# def hit_position(self, ball):
+	# 	virtual_height = self.height + ball.height
+	# 	y_dist = ball.y + ball.height - self.y
+	# 	pct = y_dist / float(virtual_height)
+	# 	return pct
+
+
+class Wall(pg.sprite.Sprite):
+    def __init__(self, x, y, width, height):
+        super().__init__()
+
+        self.width = width
+        self.height = height
+
+        self.image = pg.Surface([width, height])
+        self.image.fill((red))
+
+        self.rect = self.image.get_rect()
+        self.rect.y = y
+        self.rect.x = x
 
 class Ball(pg.sprite.Sprite):
-    def __init__(self, vector):
-        pg.sprite.Srite.__init__(self)
+
+    change_x = 0
+    change_y = 0
+    walls = None
+
+
+    def __init__(self, x, y, walls):
+        pg.sprite.Sprite.__init__(self)
         self.image = pg.image.load('hohversion/smashball.jpg')
         self.rect = self.image.get_rect()
-        self.rect.center = (BALL_X, BALL_Y)
-        self.vector = vector
+        self.rect.y = BALL_Y
+        self.rect.x = BALL_X
 
     def update(self):
-        pos = self.calcpos(self.rect, self.vector)
-        self.rect = pos
+        old_x = self.rect.x
+        new_x = old_x + self.change_x
+        self.rect = new_x
 
-    def calcpos(self, rect, vector):
-        (angle,z) = vector
-        (dx,dy) = (z*math.cos(angle), z*math.sin(angle))
-        return rect.move(dx,dy)
+        #Wall x collision
+        collide = pg.sprite.spritecollide(self, self.walls, False)
+        if collide:
+            self.rect.x = old_x
+            self.change_x *= -1
+
+        old_y = self.rect.y
+        new_y = old_y + self.change_y
+        self.rect.y = new_y
+
+        #Wall y collision
+        collide = pg.sprite.spritecollide(self, self.walls, False)
+        if collide:
+            self.rect.y = old_y
+            self.change_y *= -1
+
+        if self.rect.x < -20 or self.rect.x > screen_width + 20:
+            self.change_x = 0
+            self.change_y = 0
+
+
+    # def calcpos(self, rect, vector):
+    #     (angle,z) = vector
+    #     (dx,dy) = (z*math.cos(angle), z*math.sin(angle))
+    #     return rect.move(dx,dy)
 
 
 	
@@ -78,65 +121,89 @@ class Ball(pg.sprite.Sprite):
 #Setting the caption
 pg.display.set_caption("Smash time!")
 
-#Starting the pygame
+screen_width = 1000
+screen_height = 700
+DISPLAYSURF = pg.display.set_mode((screen_width, screen_height))
 
 
-def main():
+wall_list = pg.sprite.Group()
+all_sprites = pg.sprite.Group()
+movingsprites = pg.sprite.Group()
+#Top
+wall = Wall(0,0,screen_width,10)
+wall_list.add(wall)
+all_sprites.add(wall)
+#Bottom
+wall = Wall(0,screen_height - 10, screen_width, screen_height)
+wall_list.add(wall)
+all_sprites.add(wall)
 
-    game_over = False
+hand = MasterHand(0, 0, screen_width, 0)
 
-    hand = MasterHand(everything)
+ball = Ball(-50, -50, wall_list)
+movingsprites.add(ball)
+all_sprites.add(ball)
 
-    while True:
+while True:
 
-        x_delta = 0
-        y_delta = 0
+    x_delta = 0
+    y_delta = 0
 
-        for event in pg.event.get():
-        	if event.type == pg.QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
-        		sys.exit()
+    for event in pg.event.get():
+        if event.type == pg.QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
+            sys.exit()
 
-			# if event.type == pg.KEYDOWN:
-			# 	if event.key == pg.K_LEFT:
-			# 		#updatd x_delta and y_delta
-			# 		x_delta = 0
-			# 		y_delta = 0
-			# 		x_delta -= 15
-			# 	if event.key == pg.K_RIGHT:
-			# 		x_delta = 0
-			# 		y_delta = 0
-			# 		x_delta += 15
-			# 	if event.key == pg.K_UP:
-			# 		x_delta = 0
-			# 		y_delta = 0
-			# 		y_delta -= 15
-			# 	if event.key == pg.K_DOWN:
-			# 		x_delta = 0
-			# 		y_delta = 0
-			# 		y_delta += 15
+        elif event.type == pg.KEYDOWN:
+            if ball.change_y == 0:
+                ball.rect.x = screen_width / 2
+                ball.rect.y = random.randrange(10, screen_height - 0)
 
-        keys_pressed = pg.key.get_pressed()
-        if keys_pressed[K_LEFT]:
-            x_delta -=20
-        if keys_pressed[K_RIGHT]:
-           x_delta +=20
-# if keys_pressed[K_UP]:
-# 	y_delta -=20
-# if keys_pressed[K_DOWN]:
-# 	y_delta +=20
+                ball.change_y = random.randrange(-5,6)
+                ball.change_x = random.randrange(5, 10)
+
+                if(random.randrange(2) == 0):
+                    ball.change_x *= -1
+
+    movingsprites.update()
 
 
-        DISPLAYSURF.fill(black)
+    	# if event.type == pg.KEYDOWN:
+    	# 	if event.key == pg.K_LEFT:
+    	# 		#updatd x_delta and y_delta
+    	# 		x_delta = 0
+    	# 		y_delta = 0
+    	# 		x_delta -= 15
+    	# 	if event.key == pg.K_RIGHT:
+    	# 		x_delta = 0
+    	# 		y_delta = 0
+    	# 		x_delta += 15
+    	# 	if event.key == pg.K_UP:
+    	# 		x_delta = 0
+    	# 		y_delta = 0
+    	# 		y_delta -= 15
+    	# 	if event.key == pg.K_DOWN:
+    	# 		x_delta = 0
+    	# 		y_delta = 0
+    	# 		y_delta += 15
 
-        hand.handx+=x_delta
-        hand.handy+=y_delta
-        DISPLAYSURF.blit(hand.image, (hand.handx, hand.handy))
-        DISPLAYSURF.blit(Ball.image, (Ball.rect, Ball.vector))
-        pg.display.update()
-        fpsClock.tick(FPS)
+    keys_pressed = pg.key.get_pressed()
+    if keys_pressed[K_LEFT]:
+        x_delta -=20
+    if keys_pressed[K_RIGHT]:
+       x_delta +=20
+    # if keys_pressed[K_UP]:
+    # 	y_delta -=20
+    # if keys_pressed[K_DOWN]:
+    # 	y_delta +=20
 
-    pg.quit()
 
-if __name__ == '__main__':
-	gameExit = False
-	main()
+    DISPLAYSURF.fill(black)
+
+    hand.handx+=x_delta
+    hand.handy+=y_delta
+    DISPLAYSURF.blit(hand.image, (hand.handx, hand.handy))
+    all_sprites.draw(screen)
+    pg.display.update()
+    fpsClock.tick(FPS)
+
+pg.quit()
